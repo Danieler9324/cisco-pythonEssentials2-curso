1e0ce8097cfde9bc9eb00fdf021927ff0d669028
danieler9324 <danielviramontes562@gmail.com>
2026-01-22 20:51:46 -0600

    propiedades privadas

--- plano.py
+++ plano.py
@@ -3,20 +3,20 @@ import math
 
 class Point:
     def __init__(self, x=0.0, y=0.0):
-        self.x = x
-        self.y = y
+        self.__x = x
+        self.__y = y
 
     def getx(self):
-        return self.x
+        return self.__x
 
     def gety(self):
-        return self.y
+        return self.__y
 
     def distance_from_xy(self, x, y):
-        return math.hypot(self.x - x, self.y - y)
+        return math.hypot(self.__x - x, self.__y - y)
 
     def distance_from_point(self, point):
-        return math.hypot(self.x - point.x, self.y - point.y)
+        return math.hypot(self.__x - point.getx(), self.__y - point.gety())
 
 point1 = Point(0, 0)
 point2 = Point(1, 1)
