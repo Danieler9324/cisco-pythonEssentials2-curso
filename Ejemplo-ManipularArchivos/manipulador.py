78ef272cc9a94313da58251d9d0cc67420899200
danieler9324 <danielviramontes562@gmail.com>
2026-04-22 12:14:47 -0600

    Ejemplo de manipulacion de archivos

---
+++ Ejemplo-ManipularArchivos/manipulador.py
@@ -0,0 +1,7 @@
+stream = open("Ejemplo-ManipularArchivos/archivo.txt")
+print(stream.readlines(100))
+print(stream.readlines(100))
+print(stream.readlines(100))
+print(stream.readlines(100))
+stream.close()
+
