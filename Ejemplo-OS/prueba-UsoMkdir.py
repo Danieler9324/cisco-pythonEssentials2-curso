bf59c9fab2f95efeb6075b3442e633f3df6d334d
Danieler9324 <danielviramontes562@gmail.com>
2026-04-27 12:47:29 -0600

    se agrego try en la prueba de uso Mkdir

--- Ejemplo-OS/prueba-UsoMkdir.py
+++ Ejemplo-OS/prueba-UsoMkdir.py
@@ -1,5 +1,9 @@
 import os
-os.mkdir("./mi_primer_directorio") # Ruta relativa
-os.mkdir("../mi_primer_directorio") # Ruta relativa
-os.mkdir("Ejemplo-OS/mi_primer_directorio") # Ruta absoluta
-print(os.listdir())
+
+try:
+    os.mkdir("./mi_primer_directorio") # Ruta relativa
+    os.mkdir("../mi_primer_directorio") # Ruta relativa
+    os.mkdir("Ejemplo-OS/mi_primer_directorio") # Ruta absoluta
+    print(os.listdir())
+except FileExistsError:
+    print("El directorio ya existe en la ruta especificada")
