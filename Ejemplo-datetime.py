8eebe7f94a229877dc07c68fee8695617f580e6f
Danieler9324 <danielviramontes562@gmail.com>
2026-04-28 12:25:41 -0600

    Se agregaron ejemplos de uso de Time y replace

--- Ejemplo-datetime.py
+++ Ejemplo-datetime.py
@@ -1,8 +1,29 @@
 from datetime import date
+import time
 
+# Date
+print("----------------- Uso de date  --------------------")
 hoy = date.today()
 
 print("Hoy:", hoy)
 print("Año:", hoy.year)
 print("Mes:", hoy.month)
 print("Dia:", hoy.day)
+
+
+# Time
+print("----------------- Uso de time  --------------------")
+timestamp = time.time()
+print("Marca de tiempo:", timestamp)
+
+d = date.fromtimestamp(timestamp)
+print("Fecha:", d)
+
+# Replace
+print("----------------- Uso de replace  --------------------")
+d = date(1991, 2, 5)
+print(d)
+
+d = d.replace(year=1992, month=1, day=16)
+print(d)
+    
