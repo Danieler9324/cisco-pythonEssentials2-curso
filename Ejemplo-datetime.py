1d855f0c15c7283814672171005e58b18f990f0b
Danieler9324 <danielviramontes562@gmail.com>
2026-04-28 12:43:35 -0600

    Ejemplo de uso de weekday y isoweekday

--- Ejemplo-datetime.py
+++ Ejemplo-datetime.py
@@ -16,14 +16,21 @@ print("----------------- Uso de time  --------------------")
 timestamp = time.time()
 print("Marca de tiempo:", timestamp)
 
-d = date.fromtimestamp(timestamp)
-print("Fecha:", d)
+dia = date.fromtimestamp(timestamp) 
+print("Fecha:", dia)
 
 # Replace
 print("----------------- Uso de replace  --------------------")
-d = date(1991, 2, 5)
-print(d)
+dia2 = date(1991, 2, 5)
+print(dia2)
 
-d = d.replace(year=1992, month=1, day=16)
-print(d)
-    
+dia2 = dia2.replace(year=1992, month=1, day=16)
+print(dia2)
+
+# weekday y isoweekday
+print("----------------- Uso de weekday  --------------------")
+dia3 = date(1991, 2, 5)
+print(dia3.weekday())
+
+print("----------------- Uso de isoweekday  -----------------")
+print(dia3.isoweekday())
