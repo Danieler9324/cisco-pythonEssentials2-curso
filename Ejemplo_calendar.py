4f4877656fff4dddcdb973327a627c95a4e0c09e
Danieler9324 <danielviramontes562@gmail.com>
2026-05-04 13:45:53 -0600

    Uso de itermonthdays 2,3 y 4 agregado

--- Ejemplo_calendar.py
+++ Ejemplo_calendar.py
@@ -3,20 +3,17 @@ import calendar                                                                 
 print("==================  calendar  ===============================")                  #  -calendar.TextCalendar: se usa para crear calendario de texto regulares                            
 print(calendar.calendar(2026))                                                          #  -calendar.HTMLCalendar: se utiliza para crear calendarios HTML
                                                                                         #  -calendar.LocalTextCalendar: es una subclase de la clase calendar.TextCalendar. 
-print("===================  prcal  =================================")                  #                               El constructor de esta clase toma el parámetro locale,                              
-# prcal                                                                                 #                               el cual se utiliza para devolver los nombres 
+print("===================  prcal  =================================")                  #                               El constructor de esta clase toma el parámetro locale,                          
+                                                                                        #                               el cual se utiliza para devolver los nombres 
 calendar.prcal(2026, w=2, l=1, c=3, m=2)                                                #                               apropiados de los meses y días de la semana.
                                                                                         #  -calendar.LocalHTMLCalendar: tecnicamente es lo mismo que LocalTextCalendar pero
 print("====================  month  ================================")                  #                               ahora es la subclase de calendar.HTMLCalendar
-# month
 print(calendar.month(2026, 10))
 
 print("===================  prmonth  ===============================")
-# prmonth
 calendar.prmonth(2026, 10, w=2, l=2)
 
 print("===============  setfirstweekday  ===========================")
-# setfirstweekday
 calendar.setfirstweekday(calendar.SATURDAY)
 calendar.prmonth(2021,12)
 
@@ -36,9 +33,26 @@ c = calendar.Calendar(calendar.SUNDAY)  # 0 = MONDAY, 1 = TUESDAY, 2 = WEDNESDAY
 for weekday in c.iterweekdays():
     print(weekday, end=" ")   # = 6, 0, 1, 2, 3, 4, 5
 
-print("\n==================  itermonthdates  =========================")
-
+print("\n==================  itermonthdates  =======================")
 ca = calendar.Calendar()
 
 for fecha in ca.monthdatescalendar(2020,11):
     print(fecha, end=" \n" )
+
+print("\n==============  itermonthdays (2,3,4)  ====================")
+cal = calendar.Calendar()
+
+for itera in cal.itermonthdays(2020, 11):
+    print(itera, end=" ")
+
+print("\n")
+for itera in cal.itermonthdays2(2020, 11):
+    print(itera, end=" ")
+
+print("\n")
+for itera in cal.itermonthdays3(2020, 11):
+    print(itera, end=" ")
+
+print("\n")
+for itera in cal.itermonthdays4(2020, 11):
+    print(itera, end=" ")
